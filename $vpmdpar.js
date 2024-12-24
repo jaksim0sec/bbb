70cda336ad1ba0910b69e6811a7f9c0a80e0dea4
박영초 park0sec <155617741+jaksim0sec@users.noreply.github.com>
2024-12-24 23:25:00 +0900

    Create $vpmdpar.js

---
+++ $vpmdpar.js
@@ -0,0 +1,4 @@
+console.info("%c바보상자 커뮤니티", "color: black; font-size: 24px; font-weight: bold; padding: 5px;");
+console.log("%cBaBoBoxCrew Solo Park0sec %cver Beta %cSince 2025 :D", "color: #45d38c", "color: #7c89ff;", "color: orange;");
+console.warn("%c부적절한 콘솔코드를 함부로 작성할 경우 매우 위험할 수 있습니다.", "color: red; font-size: 10px; font-weight: bold; padding: 5px;");
+console.warn("%c만일 취약한 보안문제를 발견했다면 개발자에게 문의해주세요!!", "color: red; font-size: 10px; font-weight: bold; padding: 5px;");
