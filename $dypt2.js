25dea3b9740b85e83b1bf82178f6127f537ad870
박영초 park0sec <155617741+jaksim0sec@users.noreply.github.com>
2024-12-24 23:26:03 +0900

    Create $dypt2.js

---
+++ $dypt2.js
@@ -0,0 +1,46 @@
+function $yrdyDrmfrt(){
+  fetch('/test', {
+    method: 'POST',
+    headers: {
+      'vpz': 'yplrmEEE',
+      'zptr': JSON.stringify({
+        num: 10,
+        type: "def"
+      })
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log('$Drbrt res is', data))
+  .catch(error => console.error('$^^Ryyqy is', error));
+}
+
+function $yrdyDrmfrt2(){
+  fetch('/test', {
+    method: 'POST',
+    headers: {
+      'vpz': 'yplrm',
+      'zptr': JSON.stringify({
+        num: 1000000,
+        type: "def"
+      })
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log('$Drbrt res is', data))
+  .catch(error => console.error('$^^Ryyqy is', error));
+}
+
+function $yrdyDrmfrt1(아흣아항){
+  fetch('/test', {
+    method: 'POST',
+    headers: {
+      'vpz': 'vpfr',
+      'zptr': JSON.stringify({
+        ycy : 아흣아항
+      })
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log('$Drbrt res is', data))
+  .catch(error => console.error('$^^Ryyqy is', error));
+}
