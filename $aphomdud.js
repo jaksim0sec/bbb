1463a6ae57866e9d58cf868c306a229fc20d392d
박영초 park0sec <155617741+jaksim0sec@users.noreply.github.com>
2024-12-24 23:26:23 +0900

    Create $aphomdud.js

---
+++ $aphomdud.js
@@ -0,0 +1,41 @@
+//console.log(document.cookie);
+/*const waiter2 = setInterval(()=>{
+  //console.log(document.cookie?true:false)
+  if(true){
+    if(document.cookie.includes('$aphom')){
+      window.login=(getCok('$aphom')=='$urd')?true:false;
+      console.log('got $aphom')
+    }
+    else{
+      console.warn('Cok"s $aphom = New');
+      setCok2('$aphom=$mp;');
+      console.log('new Cok"s $aphom = $mp // $vjrvl = ',document.cookie);
+    }
+    console.log('$aphom is ',window.login);
+    clearInterval(waiter2);
+  }
+},10)*/
+
+setTimeout(()=>{
+  if(true){
+    console.log(document.cookie)
+    if(getCok('$aphom')){
+      window.login=(getCok('$aphom')=='$urd')?true:false;
+      console.log('got $aphom');
+    }
+    else{
+      console.warn('Cok"s $aphom = New');
+      if (!getCok('$aphom')) {
+        setCok2('$aphom=$mp;',5000);
+      }      
+      console.log('new Cok"s $aphom = $mp // $vjrvl = ',document.cookie);
+    }
+    console.log('$aphom is ',window.login);
+    //clearInterval(waiter2);
+  }
+},50);
+
+function $aphpiy(){
+  setCok2('$aphom=$mp;',5000);
+  location.reload();
+}
