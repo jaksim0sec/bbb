04e35ed5f3c0ecd9056336ea110e6d70bb9838fe
박영초 park0sec <155617741+jaksim0sec@users.noreply.github.com>
2024-12-25 12:19:01 +0900

    Update Login$IoDry.js

--- Login$IoDry.js
+++ Login$IoDry.js
@@ -1,1 +1,10 @@
-
+async function $$sqjpz(){
+  $NNN('UP'+'testID');
+  if (!localStorage.getItem('$$uqatz')) {
+    const $yplrm = await $NNN('UP'+'testID');
+    //console.log($yplrm,'dfasdfas')
+    localStorage.setItem('$$uqatz', $yplrm);
+  }
+  else{console.error('SUSSUSSUSSUSSUS')}
+  //console.log()
+}
